4e4ddc43f3dfb162fe8a99093e5fb0bb0a058191
Dmitry Ivanov <ivanovd@1339srv.(none)>
2008-10-02 13:38:21 +0400

    Fix #ifundef

--- crisproutingcontroller.h
+++ crisproutingcontroller.h
@@ -15,7 +15,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CRSIPROUTINGCONTROLLER_H
+#ifndef CRISPROUTINGCONTROLLER_H
 #define CRISPROUTINGCONTROLLER_H
 
 #include "odsrroutingcontroller.h"
